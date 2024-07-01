00cfcf706dc305d9867ff98570bffa4841eee8af
Srikant satapathy <srikanta.satapathy@nettantra.net>
2024-07-01 18:22:45 +0530

    implement the magic ui for skill

--- containers/Skills.tsx
+++ containers/Skills.tsx
@@ -1,29 +1,61 @@
-
-"use client"
+"use client";
 import { Icon } from "@iconify/react";
 import React, { Fragment } from "react";
 import Fade from "react-reveal/Fade";
 import { Col, Container, Row, UncontrolledTooltip } from "reactstrap";
 import DisplayLottie from "../components/DisplayLottie";
 import { skillsSection } from "../portfolio";
+import IconCloud from "../components/magicui/icon-cloud";
 
 const Skills = () => {
+  const slugs = [
+    "typescript",
+    "javascript",
+    "dart",
+    "java",
+    "react",
+    "flutter",
+    "android",
+    "html5",
+    "css3",
+    "nodedotjs",
+    "express",
+    "nextdotjs",
+    "prisma",
+    "amazonaws",
+    "postgresql",
+    "firebase",
+    "nginx",
+    "vercel",
+    "testinglibrary",
+    "jest",
+    "cypress",
+    "docker",
+    "git",
+    "jira",
+    "github",
+    "gitlab",
+    "visualstudiocode",
+    "androidstudio",
+    "sonarqube",
+    "figma",
+  ];
   return (
     skillsSection && (
       // <Fade bottom duration={2000}>
-        <Container className="text-center my-5 section section-lg">
-          <h1 className="h1">{skillsSection.title}</h1>
-          <p className="lead">{skillsSection.subTitle}</p>
-          {skillsSection.data.map((section, index) => {
-            return (
-              <Row className="my-5" key={index}>
-                <Col lg="6" className="order-2 order-lg-1">
-                  <DisplayLottie animationPath={section.lottieAnimationFile} />
-                </Col>
-                <Col lg="6" className="order-1 order-lg-2">
-                  <h3 className="h3 mb-2">{section.title}</h3>
-                  <div className="d-flex justify-content-center flex-wrap mb-2">
-                    {section.softwareSkills.map((skill, i) => {
+      <Container className="text-center my-5 section section-lg">
+        <h1 className="h1">{skillsSection.title}</h1>
+        <p className="lead">{skillsSection.subTitle}</p>
+        {skillsSection.data.map((section, index) => {
+          return (
+            <Row className="my-5" key={index}>
+              {/* <Col lg="6" className="order-2 order-lg-1">
+                <DisplayLottie animationPath={section.lottieAnimationFile} />
+              </Col> */}
+              <Col lg="12" className="order-1 order-lg-2">
+                <h3 className="h3 mb-2">{section.title}</h3>
+                <div className="d-flex justify-content-center flex-wrap mb-2">
+                  {/* {section.softwareSkills.map((skill, i) => {
                       return (
                         <Fragment key={i}>
                           <div
@@ -37,18 +69,21 @@ const Skills = () => {
                           </UncontrolledTooltip>
                         </Fragment>
                       );
-                    })}
+                    })} */}
+                  <div className="relative flex h-[40%] w-full max-w-[32rem] items-center justify-center overflow-hidden ">
+                    <IconCloud iconSlugs={slugs} />
                   </div>
-                  <div>
-                    {section.skills.map((skill, i) => {
-                      return <p key={i}>{skill}</p>;
-                    })}
-                  </div>
-                </Col>
-              </Row>
-            );
-          })}
-        </Container>
+                </div>
+                <div>
+                  {section.skills.map((skill, i) => {
+                    return <p key={i}>{skill}</p>;
+                  })}
+                </div>
+              </Col>
+            </Row>
+          );
+        })}
+      </Container>
       //  </Fade>
     )
   );
